c5e5fe93afedcf2cafe485687235a710620f4877
Kaushik das <kaushik1641.cse19@chitkara.edu.in>
2022-11-10 21:18:31 +0530

    update02

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const {Server} = require('socket.io');
+
+const app = express();
+const http = require('http');
+
+const server = http.createServer(app);
+const io = new Server(server);
+
+
+io.on('connection', (socket) => {
+    console.log('Socket Connected', socket.id)
+})
+
+
+
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => console.log(`Listening on port ${PORT}`))
---
+++ src/components/Editor.js
@@ -0,0 +1,31 @@
+import React, { useEffect } from 'react'
+import Codemirror from 'codemirror';
+import 'codemirror/lib/codemirror.css';
+import  'codemirror/theme/dracula.css'
+import 'codemirror/mode/javascript/javascript'
+import 'codemirror/addon/edit/closetag'
+import 'codemirror/addon/edit/closebrackets'
+
+
+const Editor = () => {
+
+    const init = async() => {
+        Codemirror.fromTextArea(document.getElementById('realtimeEditor'), {
+            mode: {name: 'javascript', json: true},
+            theme: 'dracula',
+            autoCloseTags:true,
+            autoCloseBrackets:true,
+            lineNumbers: true,
+        })
+    }
+
+    useEffect(() => {
+        init();
+    },[])
+
+  return (
+    <textarea id='realtimeEditor'></textarea>
+  )
+}
+
+export default Editor
