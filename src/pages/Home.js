e85ef96c4c8534c2840dcdb545529b708389f813
Kaushik das <kaushik1641.cse19@chitkara.edu.in>
2022-11-11 13:21:47 +0530

    up03

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,28 +1,27 @@
-import React, { useState } from "react";
-import { v4 as uuidV4 } from "uuid";
-import toast from "react-hot-toast";
-import { useNavigate } from "react-router-dom";
+import React, { useState } from 'react';
+import { v4 as uuidV4 } from 'uuid';
+import toast from 'react-hot-toast';
+import { useNavigate } from 'react-router-dom';
 
 const Home = () => {
-    const [roomId, setRoomId] = useState("");
-    const [username, setUsername] = useState("");
     const navigate = useNavigate();
 
+    const [roomId, setRoomId] = useState('');
+    const [username, setUsername] = useState('');
     const createNewRoom = (e) => {
         e.preventDefault();
         const id = uuidV4();
         setRoomId(id);
-
-        toast.success("Room created successfully");
+        toast.success('Created a new room');
     };
 
     const joinRoom = () => {
         if (!roomId || !username) {
-            toast.error("Please enter room id and username");
+            toast.error('ROOM ID & username is required');
             return;
         }
 
-        //Redirect after join
+        // Redirect
         navigate(`/editor/${roomId}`, {
             state: {
                 username,
@@ -30,12 +29,11 @@ const Home = () => {
         });
     };
 
-    const handleEnter = (e) => {
-        if (e.key === "Enter") {
+    const handleInputEnter = (e) => {
+        if (e.code === 'Enter') {
             joinRoom();
         }
     };
-
     return (
         <div className="homePageWrapper">
             <div className="formWrapper">
@@ -44,52 +42,45 @@ const Home = () => {
                     src="/code-sync.png"
                     alt="code-sync-logo"
                 />
-                <h4 className="mainLabel"> Paste invitation ROOM ID </h4>
-
+                <h4 className="mainLabel">Paste invitation ROOM ID</h4>
                 <div className="inputGroup">
                     <input
                         type="text"
-                        placeholder="ROOM ID"
                         className="inputBox"
-                        value={roomId}
+                        placeholder="ROOM ID"
                         onChange={(e) => setRoomId(e.target.value)}
-                        onKeyUp={handleEnter}
-                    />{" "}
+                        value={roomId}
+                        onKeyUp={handleInputEnter}
+                    />
                     <input
                         type="text"
-                        placeholder="USERNAME"
                         className="inputBox"
+                        placeholder="USERNAME"
                         onChange={(e) => setUsername(e.target.value)}
                         value={username}
-                        onKeyUp={handleEnter}
-                    />{" "}
-                    <button className="joinBtn btn" onClick={joinRoom}>
-                        {" "}
-                        Join{" "}
+                        onKeyUp={handleInputEnter}
+                    />
+                    <button className="btn joinBtn" onClick={joinRoom}>
+                        Join
                     </button>
                     <span className="createInfo">
-                        {" "}
-                        If you don 't have an invite then create &nbsp;{" "}
-                        <a onClick={createNewRoom} href="" className="createNewBtn">
-                            new room{" "}
-                        </a>{" "}
-                    </span>{" "}
+                        If you don't have an invite then create &nbsp;
+                        <a
+                            onClick={createNewRoom}
+                            href=""
+                            className="createNewBtn"
+                        >
+                            new room
+                        </a>
+                    </span>
                 </div>
             </div>
             <footer>
                 <h4>
-                    {" "}
-                    Built with💛 by & nbsp;{" "}
-                    <a
-                        href="https://github.com/kaushikd-coder"
-                        target="_blank"
-                        rel="noreferrer"
-                    >
-                        {" "}
-                        Kaushik{" "}
-                    </a>{" "}
-                </h4>{" "}
-            </footer>{" "}
+                    Built with 💛 by &nbsp;
+                    <a  href="https://github.com/kaushikd-coder" target="_blank" rel="noreferrer">Kaushik</a>
+                </h4>
+            </footer>
         </div>
     );
 };
